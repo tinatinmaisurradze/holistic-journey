0f0e94aa767e401e67b01e15207bb81e090ff378
tinatinmaisurradze <tinatin.maisuradze@tsc.edu.ge>
2026-02-06 01:45:04 +0400

    Add Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['var(--font-serif)', 'serif'],
+        sans: ['var(--font-sans)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
